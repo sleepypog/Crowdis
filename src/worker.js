96ce39b1a8df960ace935837a2c38aa1739c0ae6
Gabriel Arriagada <65878291+GNosii@users.noreply.github.com>
2022-11-18 15:20:15 +0000

    refactor: code refactor in worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,20 +1,18 @@
-import { setupLanguage, getSummary } from "./strings";
+import { normalize, setupLanguage, string } from "./strings";
 
 // Webhook avatar and username, displayed on Discord
-const WEBHOOK_USERNAME = "Crowdin Updates";
-const WEBHOOK_AVATAR = "";
+const WEBHOOK_USERNAME = "Crowdis";
+const WEBHOOK_AVATAR = "https://support.crowdin.com/assets/logos/crowdin-dark-symbol.png";
 
 // Base webhook endpoint
-const WEBHOOK_BASE_URL = "https://discordapp.com/api/webhooks%s"
+const WEBHOOK_BASE_URL = "https://discordapp.com/api/webhooks"
 
 addEventListener('fetch', (event) => {
-    console.debug('EventListener');
-
     if (event.request.method !== "POST") {
         return event.respondWith(new Response(
             'This endpoint only accepts POST requests',
             {
-                status: 400
+                status: 400,
             }
         ));
     }
@@ -33,26 +31,101 @@ addEventListener('fetch', (event) => {
 });
 
 async function handleFetch(request) {
-    await setupLanguage();
+    setupLanguage();
 
-    const url = new URL(request.url)
+    const { pathname, searchParams } = new URL(request.url)
 
-    const preferRaw = url.searchParams.has('raw');
-    const preferEmbed = !preferRaw && url.searchParams.has('embed');
+    if (pathname === "/") {
+        return new Response(
+            'Invalid endpoint.',
+            {
+                status: 400,
+            }
+        );
+    }
+
+    const preferEmbed = searchParams.has('embed');
 
     const data = await request.json();
 
     let events = [];
 
     if (data.event !== undefined)
-        events = [ data ];
+        events = [data];
     else
         events = data.events;
 
+    const summaries = [];
     events.forEach((event) => {
-        const summary = getSummary(event);
-        console.debug('Summary: ' + summary)
+        summaries.push(string(event.event, event));
     });
 
-    return new Response();
+    let body;
+    
+    if (preferEmbed) {
+        body = {
+            embeds: [
+                {
+                    title: string('webhook.header'),
+                    description: normalize(summaries),
+                    url: null,
+                    timestamp: new Date(Date.now()).toISOString(),
+                    color: null,
+                    footer: {
+                        text: null,
+                        icon_url: null
+                    },
+                    image: {
+                        url: null
+                    },
+                    thumbnail: {
+                        url: null
+                    },
+                    author: {
+                        name: null,
+                        url: null,
+                        icon_url: null
+                    },
+                    fields: []
+                }
+            ]
+        }
+    } else {
+        body = {
+            "content": normalize([
+                string('webhook.header', { project: events[0].project }),
+            ].concat(summaries))
+        }
+    }
+
+    try {
+        await sendWebhook(pathname, body);
+    } catch (err) {
+        console.error('Error while sending webhook: ' + err.toString())
+        return new Response(
+            'Internal error',
+            {
+                status: 500
+            }
+        )
+    }
+
+    return new Response('Processed successfully', {
+        status: 200
+    });
+}
+
+async function sendWebhook(path, content) {
+    const auth = {
+        username: WEBHOOK_USERNAME,
+        avatar_url: WEBHOOK_AVATAR,
+    }
+    
+    return fetch(WEBHOOK_BASE_URL + path, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Object.assign(content, auth))
+    })
 }
